8b2ad7fae58c8d21c2e5389243895048af6de667
shreedhar desai <desaishreedhar09@gmail.com>
2020-05-30 18:24:09 +0530

    Updates

---
+++ precache-manifest.879497b8df251ad91db9b39ef95422a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf999115d609c6099b609a8bde85744",
+    "url": "/User_Info/index.html"
+  },
+  {
+    "revision": "6100cc048d1dc9065278",
+    "url": "/User_Info/static/css/2.3d69e9be.chunk.css"
+  },
+  {
+    "revision": "241554f0cd38f46b7a28",
+    "url": "/User_Info/static/css/main.285a17c0.chunk.css"
+  },
+  {
+    "revision": "6100cc048d1dc9065278",
+    "url": "/User_Info/static/js/2.04cee6e8.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/User_Info/static/js/2.04cee6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241554f0cd38f46b7a28",
+    "url": "/User_Info/static/js/main.fdc66923.chunk.js"
+  },
+  {
+    "revision": "8dd0cefb67bfe8c98017",
+    "url": "/User_Info/static/js/runtime-main.8d982dc3.js"
+  },
+  {
+    "revision": "afef97da053f2eb3c381e2fc5bfe1107",
+    "url": "/User_Info/static/media/LoginBackGround.afef97da.png"
+  }
+]);
